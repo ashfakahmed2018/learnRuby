297391fc0bb084108835e7773be061a6c630cbad
ashfakahmed2018 <ccl.ashfaqbd@yahoo.com>
2019-07-12 03:07:30 +0000

    new change

--- firstruby.rb
+++ firstruby.rb
@@ -4,19 +4,6 @@
 
 $global_variable = 10
 
-class Customer
-    def initialize(id, name, address)
-        @customer_id = id
-        @customer_name = name
-        @customer_address = address
-    end
-    def display_details()
-        puts "customer_id #@customer_id"
-        puts "customer_name #@customer_name"
-        puts "customer_address #@customer_address"
-    end
-end
-
 class Sample
     def hello
         puts "Hello Ashfak! #$global_variable"
@@ -29,8 +16,25 @@ class Car
         puts "infiniti has more powerful engin then other car - #$global_variable"
     end
 end
-        
-        
+
+class Customer
+    @@no_of_customers = 0
+    def initialize(id, name, address)
+        @customer_id = id
+        @customer_name = name
+        @customer_address = address
+    end
+    def display_details()
+        puts "customer_id #@customer_id"
+        puts "customer_name #@customer_name"
+        puts "customer_address #@customer_address"
+    end
+    def total_no_of_customers()
+        @@no_of_customers += 1
+        puts "Total number of customers: #@@no_of_customers"
+    end
+end
+
 # Using above class
 
 object = Sample. new
@@ -42,8 +46,11 @@ object.Luxery
 customer1 = Customer.new("1", "Ashfak", "108-03 160th street, jamaica")
 customer2 = Customer.new("2", "Nitu", "chedarcraft road, jamaica")
 
-customer1.display_details()
-customer2.display_details()
+
+
+customer1.total_no_of_customers()
+customer2.total_no_of_customers()
+
 
 
 
