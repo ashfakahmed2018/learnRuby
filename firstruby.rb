9f44e79c2ac69accca0b605613371d8d77852443
ashfakahmed2018 <ccl.ashfaqbd@yahoo.com>
2019-07-12 01:52:08 +0000

    Test commit

---
+++ firstruby.rb
@@ -0,0 +1,24 @@
+#!/user/bin/ruby
+
+class Sample
+    def hello
+        puts "Hello Ashfak!"
+    end
+end
+
+class Car
+    def Luxery
+        puts "Infiniti is a luxery car"
+        puts "infiniti has more powerful engin then other car"
+    end
+end
+        
+        
+# Using above class to create objects
+
+object = Sample. new
+object.hello
+
+object = Car. new
+object.Luxery
+    
